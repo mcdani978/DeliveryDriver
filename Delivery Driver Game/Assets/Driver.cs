c500eebc4e4a3bd0bfd1a5f5ba87da64a60844e5
mcdani978 <mcdani978@morrisville.edu>
2023-09-06 10:15:35 -0400

    Introducing Variables

--- Delivery Driver Game/Assets/Driver.cs
+++ Delivery Driver Game/Assets/Driver.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    float steerSpeed = 1f;
+    float moveSpeed = 0.01f; 
+    
     void Start()
     {
         
@@ -13,7 +16,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0,0.01f,0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
