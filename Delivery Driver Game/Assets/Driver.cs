17d0ac39e4419b1461f8df40148e2ef6daafd3eb
mcdani978 <mcdani978@morrisville.edu>
2023-09-12 23:09:18 -0400

    Boosts & Bumps

--- Delivery Driver Game/Assets/Driver.cs
+++ Delivery Driver Game/Assets/Driver.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f; 
     
-    void Start()
-    {
-        
-    }
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+    
+    
 
-    // Update is called once per frame
+    
     void Update()
     {
         float steerAmount = UnityEngine.Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = UnityEngine.Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
