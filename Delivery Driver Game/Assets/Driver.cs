08d9f09f7027a4cd035c0ba199d748e2b271fc86
mcdani978 <mcdani978@morrisville.edu>
2023-12-10 14:15:09 -0500

    Player Always Stays Within camera while the camera moves Foreward

--- Delivery Driver Game/Assets/Driver.cs
+++ Delivery Driver Game/Assets/Driver.cs
@@ -1,33 +1,32 @@
+// Driver script
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    
     [SerializeField] float Flt_steerSpeed = 1f;
     [SerializeField] float Flt_moveSpeed = 20f;
     [SerializeField] float Flt_slowSpeed = 15f;
     [SerializeField] float Flt_boostSpeed = 30f;
-    
-    
 
-    
     void Update()
     {
         float Flt_steerAmount = UnityEngine.Input.GetAxis("Horizontal") * Flt_steerSpeed * Time.deltaTime;
         float Flt_moveAmount = UnityEngine.Input.GetAxis("Vertical") * Flt_moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -Flt_steerAmount);
         transform.Translate(0, Flt_moveAmount, 0);
-    }
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        Flt_moveSpeed = Flt_slowSpeed;
+
+        // Clamp the player's position to the camera's boundaries
+        Vector3 position = Camera.main.WorldToViewportPoint(transform.position);
+        position.x = Mathf.Clamp(position.x, 0.1f, 0.9f);
+        position.y = Mathf.Clamp(position.y, 0.1f, 0.9f);
+        transform.position = Camera.main.ViewportToWorldPoint(position);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Boost")
+        if (other.tag == "Boost")
         {
             Flt_moveSpeed = Flt_boostSpeed;
         }
--- Delivery Driver Game/Assets/Followcamera.cs
+++ Delivery Driver Game/Assets/Followcamera.cs
@@ -1,16 +1,27 @@
+// Followcamera script
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Followcamera : MonoBehaviour
 {
-    [SerializeField] GameObject thingToFollow;
-    // this things position (camera) should be the same as the cars position
-    
+    [SerializeField] float cameraSpeed = 5f; // Speed at which the camera moves forward
+    [SerializeField] GameObject topCollider; // The collider that prevents the player from moving off-screen
 
-    
+    void Update()
+    {
+        // Adjust the size of the collider to match the camera's view
+        float colliderWidth = Camera.main.aspect * Camera.main.orthographicSize * 2;
+        topCollider.GetComponent<BoxCollider2D>().size = new Vector2(colliderWidth, 1);
+    }
+
     void LateUpdate()
     {
-       transform.position = thingToFollow.transform.position + new Vector3 (0,0,-10);
+        // Calculate the new position of the camera
+        Vector3 newPosition = transform.position + new Vector3(0, cameraSpeed * Time.deltaTime, 0);
+        // Update the camera's position
+        transform.position = newPosition;
+        // Move the top collider with the camera
+        topCollider.transform.position = new Vector3(transform.position.x, transform.position.y + Camera.main.orthographicSize, transform.position.z);
     }
 }
